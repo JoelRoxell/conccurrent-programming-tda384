4953d5c490af6f45a5facfb03b2e4f59130396cd
noone <joel.roxell@annevo.se>
2018-09-18 14:30:52 +0200

    Train: added some more comments.

--- trainspotting/src/main/java/se/chalmers/train/Train.java
+++ trainspotting/src/main/java/se/chalmers/train/Train.java
@@ -11,11 +11,21 @@ public class Train implements Runnable {
     private int id;
     private int speed;
 
+    // acquired keeps track of the current semaphores that the train / thread has
+    // locked.
     private ArrayList<Semaphore> acquired = new ArrayList<Semaphore>();
 
+    // semaphores is used to be aware of all semaphores placed on the map.
     private ArrayList<Semaphore> semaphores;
+
+    // sensors is used to be aware of all sensors placed on the map.
     private Map<Point, Semaphore[]> sensors;
+
+    // Switches is used to know where all switches are on the map.
     private Map<Point, Point> switches;
+
+    // tsi points to the singleton reference that is used to communiated with
+    // `tsim`.
     private TSimInterface tsi;
 
     public Train(int id, int speed, int start_point, Semaphore[] sems, Map<Point, Semaphore[]> sens,
@@ -49,6 +59,7 @@ public class Train implements Runnable {
         while (true) {
             try {
                 SensorEvent ev = tsi.getSensor(id);
+
                 resolve(ev);
             } catch (InterruptedException | CommandException e) {
                 e.printStackTrace();
@@ -72,6 +83,7 @@ public class Train implements Runnable {
     private void resolve(SensorEvent ev) throws InterruptedException, CommandException {
         Point sensor = new Point(ev.getXpos(), ev.getYpos());
         Semaphore[] sems = sensors.get(sensor);
+
         if (ev.getStatus() == SensorEvent.ACTIVE) {
             if (sems == null) {
                 reverse();
@@ -84,6 +96,14 @@ public class Train implements Runnable {
         }
     }
 
+    /**
+     * Allows the train to release a number of semaphores (1-2 in this case) passed
+     * via sems, the train will only release semaphores that have been acuired
+     * previously.
+     * 
+     * @param sems
+     * @return
+     */
     private boolean release(Semaphore[] sems) {
         for (int i = 0; i < sems.length; i++) {
             if (acquired.contains(sems[i])) {
@@ -95,6 +115,14 @@ public class Train implements Runnable {
         return false;
     }
 
+    /**
+     * Allows the train to aquire one or more semaphores, if it can't, the train
+     * will stop and wait until either of the semaphores are ready to be acuired.
+     * 
+     * @param sems
+     * @throws InterruptedException
+     * @throws CommandException
+     */
     private void acquire(Semaphore[] sems) throws InterruptedException, CommandException {
         if (sems[0].tryAcquire()) {
             acquired.add(sems[0]);
@@ -119,8 +147,10 @@ public class Train implements Runnable {
      */
     private void redirect(Point sensor) throws CommandException {
         Point swi = switches.get(sensor);
+
         int track1 = semaphores.indexOf(acquired.get(0));
         int track2 = semaphores.indexOf(acquired.get(1));
+
         if (swi != null) {
             if ((track1 + track2) % 2 == 0)
                 tsi.setSwitch(swi.x, swi.y, TSimInterface.SWITCH_LEFT);
@@ -131,6 +161,7 @@ public class Train implements Runnable {
 
     private void setSpeed(int speed) throws CommandException, InterruptedException {
         tsi.setSpeed(this.id, speed);
+
         this.speed = speed;
     }
 
@@ -140,7 +171,9 @@ public class Train implements Runnable {
 
     private void reverse() throws CommandException, InterruptedException {
         stop();
+
         Thread.sleep(1000 + (20 * Math.abs(speed)));
+
         setSpeed(-speed);
     }
 
